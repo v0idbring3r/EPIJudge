40af64f7a33391422f9221fc8f50c1ff756d940d
Angad Pal Singh <angadps@microsoft.com>
2020-02-18 08:25:59 +0530

    Big int increment 2

--- epi_judge_cpp/int_as_array_increment.cc
+++ epi_judge_cpp/int_as_array_increment.cc
@@ -4,21 +4,33 @@
 using std::vector;
 vector<int> PlusOne(vector<int> A) {
   // TODO - you fill in here.
-    auto result = vector<int>{};
-    int carry = 1, i = A.size() - 1;
+//    auto result = vector<int>{};
+//    int carry = 1, i = A.size() - 1;
+//
+//    while (carry > 0 && i >= 0) {
+//        result.push_back((A[i]+1)%10);
+//        carry = (A[i--]+1)/10;
+//    }
+//    while (i >= 0) {
+//        result.push_back(A[i--]);
+//    }
+//    if (carry > 0)
+//        result.push_back(1);
+//    std::reverse(result.begin(), result.end());
+//
+//    return result;
+    auto i = A.size() - 1;
     
-    while (carry > 0 && i >= 0) {
-        result.push_back((A[i]+1)%10);
-        carry = (A[i--]+1)/10;
+    while (i >= 0 && A[i] == 9) {
+        A[i--] = 0;
     }
-    while (i >= 0) {
-        result.push_back(A[i--]);
-    }
-    if (carry > 0)
-        result.push_back(1);
-    std::reverse(result.begin(), result.end());
-
-    return result;
+    
+    if (i == -1)
+        A.insert(A.begin(), 1);
+    else
+        ++A[i];
+    
+    return A;
 }
 
 int main(int argc, char* argv[]) {

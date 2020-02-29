1275f5f06306e30dec0ea42f606ad24503ca233c
Angad Pal Singh <angadps@microsoft.com>
2020-02-29 10:41:01 +0530

    Prime sieve

--- epi_judge_cpp/buy_and_sell_stock_twice.cc
+++ epi_judge_cpp/buy_and_sell_stock_twice.cc
@@ -2,8 +2,22 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 double BuyAndSellStockTwice(const vector<double>& prices) {
-  // TODO - you fill in here.
-  return 0.0;
+    double max_profit = 0, minimum_stock_so_far = std::numeric_limits<double>::max();
+    vector<double> max_profits(prices.size(), 0);
+    
+    for (auto i = 0; i < prices.size(); ++i) {
+        max_profit = std::max(max_profit, prices[i] - minimum_stock_so_far);
+        minimum_stock_so_far = std::min(minimum_stock_so_far, prices[i]);
+        max_profits[i] = max_profit;
+    }
+    
+    auto max_price_so_far = std::numeric_limits<double>::min();
+    for (auto i = prices.size() - 1; i >= 0; --i) {
+        max_profit = std::max(max_profit, max_profits[i] + max_price_so_far - prices[i]);
+        max_price_so_far = std::max(max_price_so_far, prices[i]);
+    }
+    
+    return max_profit;
 }
 
 int main(int argc, char* argv[]) {
--- epi_judge_cpp/prime_sieve.cc
+++ epi_judge_cpp/prime_sieve.cc
@@ -3,8 +3,27 @@
 using std::vector;
 // Given n, return all primes up to and including n.
 vector<int> GeneratePrimes(int n) {
-  // TODO - you fill in here.
-  return {};
+    int size = std::floor(0.5*(n-3)) + 1;
+    vector<bool> is_prime(size, true);
+    vector<int> result;
+    
+    if (n >= 2)
+        result.emplace_back(2);
+    for (long i = 0; i < size; ++i) {
+        if (is_prime[i]) {
+            const long p = 2*i + 3;
+            result.emplace_back(p);
+            for (long j = p*p; j <= n; j += 2*p) {
+                const long x = (j-3)/2;
+                is_prime[x] = false;
+            }
+//            for (long j = 2*i*i + 6*i + 3; j < size; j += p) {
+//                is_prime[j] = false;
+//            }
+        }
+    }
+    
+    return result;
 }
 
 int main(int argc, char* argv[]) {

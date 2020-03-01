cf637cf508cd5b891b1684fa5216648c003d967f
Angad Pal Singh <angadps@microsoft.com>
2020-03-01 19:26:03 +0530

    next permutation

--- epi_judge_cpp/next_permutation.cc
+++ epi_judge_cpp/next_permutation.cc
@@ -2,8 +2,24 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 vector<int> NextPermutation(vector<int> perm) {
-  // TODO - you fill in here.
-  return {};
+    int i = perm.size()-2;
+    for (; i >= 0; --i) {
+        if (perm[i] < perm[i+1]) {
+            for(int j = perm.size()-1; j > i; --j) {
+                if (perm[j] > perm[i]) {
+                    std::swap(perm[i], perm[j]);
+                    break;
+                }
+            }
+            break;
+        }
+    }
+    
+    if (i < 0)
+        return {};
+
+    std::reverse(perm.begin()+i+1, perm.end());
+    return perm;
 }
 
 int main(int argc, char* argv[]) {
